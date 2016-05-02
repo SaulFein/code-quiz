cc8199750d073cbbb75b79762247da33f8427665
nienowt <elstad.nienow@gmail.com>
2016-05-02 12:12:13 -0700

    setting up

---
+++ app/controllers/question-controller.js
@@ -0,0 +1,14 @@
+module.exports = function(app) {
+  app.controller('QuestionController',['$http', function($http){
+    let url = 'http://localhost:3000/api/questions'
+    this. allQuestions = ['dumb']
+
+    this.getQuestions = function(){
+      $http.get(url)
+        .then((res) => {
+          console.log(res)
+        })
+    }
+
+  }])
+}
