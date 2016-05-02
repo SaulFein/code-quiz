47acff707ac9591a133e66dc5a358e20b8e6f75d
nienowt <elstad.nienow@gmail.com>
2016-05-02 13:17:39 -0700

    added cors/questcontroller

--- app/controllers/question-controller.js
+++ app/controllers/question-controller.js
@@ -1,13 +1,26 @@
 module.exports = function(app) {
   app.controller('QuestionController',['$http', function($http){
     let url = 'http://localhost:3000/api/questions'
-    this.allQuestions = ['dumb']
+    this.allQuestions = [];
+    this.catQuestions = [];
 
     this.getQuestions = function(){
       $http.get(url)
         .then((res) => {
-          console.log(res)
+          this.allQuestions = res.data.data;
+          console.log(res.data.data)
+          console.log('made it here?')
         })
+    }
+    this.getCategory = function(category){
+      this.catQuestions = this.allQuestions.filter((q) => {
+        return q.category == category;
+      })
+    }
+    this.getDifficulty = function(difficulty){
+      this.catQuestions = this.catQuestions.filter((q) => {
+        return q.difficulty == difficulty
+      })
     }
 
   }])
