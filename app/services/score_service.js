5de96d298539e1764e409f2a457fae35981938e6
jalynnlim <jalynnlim@gmail.com>
2016-05-03 13:45:50 -0700

    added token to score service

--- app/services/score_service.js
+++ app/services/score_service.js
@@ -1,29 +1,45 @@
 'use strict';
 
 module.exports = function(app) {
-  app.factory('ScoreService', ['$http', function($http) {
+  app.factory('ScoreService', ['$http', 'AuthService', function($http, AuthService) {
     const mainRoute = "http://localhost:3000/api/";
 
     var scoreService = {};
 
-    scoreService.createScore = function(userId) {
-      return $http.post(mainRoute + '/users/' + userId + '/scores', data);
+    scoreService.createScore = function(data) {
+      return $http.post(mainRoute + '/users/' + data.userId + '/scores', data);
     };
 
-    scoreService.getScores = function(data) {
-      return $http.get(mainRoute + '/users/' + data.userId + '/scores')
+    scoreService.getScores = function(userId) {
+      return $http.get(mainRoute + '/users/' + userId + '/scores', {
+        headers: {
+          token: AuthService.getToken();
+        }
+      })
     };
 
     scoreService.getScore = function(data) {
-      return $http.get(mainRoute + '/users/' + data.userId + '/scores/' + data.scoreId);
+      return $http.get(mainRoute + '/users/' + data.userId + '/scores/' + data.scoreId, {
+        headers: {
+          token: AuthService.getToken();
+        }
+      });
     };
 
     scoreService.updateScore = function(data) {
-      return $http.put(mainRoute + '/users/' + data.userId + '/scores/' + data.scoreId, data);
+      return $http.put(mainRoute + '/users/' + data.userId + '/scores/' + data.scoreId, data, {
+        headers: {
+          token: AuthService.getToken();
+        }
+      });
     };
 
     scoreService.resetScore = function(data) {
-      return $http.delete(mainRoute + '/users/' + data.userId + '/scores/' + data.scoreId);
+      return $http.delete(mainRoute + '/users/' + data.userId + '/scores/' + data.scoreId, {
+        headers: {
+          token: AuthService.getToken();
+        }
+      });
     };
 
     return scoreService;
