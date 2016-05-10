cf870b2778dbb63fff97e61c1fe39c65f8f6abd1
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2016-05-10 12:52:37 -0700

    test fix

--- app/services/score_service.js
+++ app/services/score_service.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
     let scoreService = {};
 
     scoreService.createScore = function(data) {
-      return $http.post('/users/' + data.userId + '/scores', data)
+      return $http.post('/api/users/' + data.userId + '/scores', data)
       .then((res)=>{
         scoreId = $window.localStorage.scoreId = res.data.data._id;
         console.log(res);
@@ -15,7 +15,7 @@ module.exports = function(app) {
     };
 
     scoreService.getScores = function(userId) {
-      return $http.get('/users/' + userId + '/scores', {
+      return $http.get('/api/users/' + userId + '/scores', {
         headers: {
           token: AuthService.getToken()
         }
@@ -23,7 +23,7 @@ module.exports = function(app) {
     };
 
     scoreService.getScore = function(data) {
-      return $http.get('/users/' + data.userId + '/scores/' + data.scoreId, {
+      return $http.get('/api/users/' + data.userId + '/scores/' + data.scoreId, {
         headers: {
           token: AuthService.getToken()
         }
@@ -31,7 +31,7 @@ module.exports = function(app) {
     };
 
     scoreService.getScoreId = function(data) {
-      return $http.get('/users/' + data.userId + '/scores?category=' + data.category + '&difficulty=' + data.difficulty, {
+      return $http.get('/api/users/' + data.userId + '/scores?category=' + data.category + '&difficulty=' + data.difficulty, {
         headers: {
           token: AuthService.getToken()
         }
@@ -40,7 +40,7 @@ module.exports = function(app) {
 
     scoreService.updateScore = function(data, scoreId) {
       console.log(scoreId)
-      return $http.put('/users/' + data.userId + '/scores/' + scoreId, data, {
+      return $http.put('/api/users/' + data.userId + '/scores/' + scoreId, data, {
         headers: {
           token: AuthService.getToken()
         }
@@ -48,7 +48,7 @@ module.exports = function(app) {
     };
 
     scoreService.resetScore = function(data) {
-      return $http.delete('/users/' + data.userId + '/scores/' + data.scoreId, {
+      return $http.delete('/api/users/' + data.userId + '/scores/' + data.scoreId, {
         headers: {
           token: AuthService.getToken()
         }
