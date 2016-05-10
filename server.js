a9ef3827c49247cb99628bb929f8affe1f6cadab
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2016-05-09 20:35:02 -0700

    trying ish

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ let config = require(__dirname + '/config/env.js');
 app.use(express.static(__dirname + '/build'));
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+  res.header('Access-Control-Allow-Origin', 'https://localhost:8080');
   res.header('Access-Control-Allow-Headers', 'Content-Type, token, authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
